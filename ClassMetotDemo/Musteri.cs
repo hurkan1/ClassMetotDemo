c86e805ad9082a2176acd6d64bef4cd29177242f
huaydin <hurkanaydin@gmail.com>
2021-01-26 16:36:53 +0300

    ClassMethodDemo

--- ClassMetotDemo/Musteri.cs
+++ ClassMetotDemo/Musteri.cs
@@ -8,7 +8,7 @@ namespace ClassMetotDemo
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public int Id { get; set; }
+        public string Id { get; set; }
         public int Age { get; set; }
 
 
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,37 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Name = "Hürkan";
+            musteri1.Surname = "Aydın";
+            musteri1.Age = 30;
+            musteri1.Id = "1";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Name = "Ayşe";
+            musteri2.Surname = "Aydın";
+            musteri2.Age = 40;
+            musteri2.Id = "2";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Name = "Elif";
+            musteri3.Surname = "Aydın";
+            musteri3.Age = 50;
+            musteri3.Id = "3";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Name + " " + musteri.Surname + " " + musteri.Age + " " + musteri.Id + " ");
+
+                Console.WriteLine("--------------");
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.List(musteri2);
+            musteriManager.Del(musteri3);
         }
     }
 }
